06687b5a4cd3dafd605a1d249400168e9f5fb7c3
Brandon Hornick <158857799+bhornick09@users.noreply.github.com>
2025-09-10 13:50:29 -0400

    Add files via upload

---
+++ TCG Card Shop Mod Menu Code/HarmonyPatches.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace TCGCardPandL
+{
+    // God Pack (change random number to god pack key)
+    [HarmonyPatch(typeof(CardOpeningSequence), "GetPackContent")]
+    public static class GodPackPatch
+    {
+        // Use access tools to access the card pack type 
+        static FieldInfo collectionPackTypeField = AccessTools.Field(typeof(CardOpeningSequence), "m_CollectionPackType");
+
+        static void Prefix(CardOpeningSequence __instance, ref int godPackRollIndex, ref ECollectionPackType overrideCollectionPackType)
+        {
+            // if Normal is not selected, override pack odds with selection
+            if (Plugin.Instance.godPackSelection.Value != GodPackMode.Normal)
+            {
+                // check for type ghost, which overrides godPackRollIndex if true
+                if (Plugin.Instance.godPackSelection.Value == GodPackMode.Ghost)
+                {
+                    collectionPackTypeField.SetValue(__instance, ECollectionPackType.GhostPack);
+                }
+                // if not ghost push other pack type
+                else
+                {
+                    GodPackMode packTypeSelection = Plugin.Instance.godPackSelection.Value;
+                    godPackRollIndex = (int)packTypeSelection; //convert to int for index use
+                }
+            }
+        }
+    }
+
+    // Override checkout total
+    [HarmonyPatch(typeof(FurnitureShopUIScreen), "EvaluateCartCheckout")]
+    public static class EvaluateCartCheckoutPatch
+    {
+
+        static void Prefix(ref float totalCost)
+        {
+            if (Plugin.Instance.freeShopItems.Value)
+            {
+                // Override checkout total to 0
+                totalCost = 0.0f;
+            }
+        }
+    }
+}
---
+++ TCG Card Shop Mod Menu Code/Plugin.cs
@@ -0,0 +1,104 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using UnityEngine;
+using System.ComponentModel;
+
+namespace TCGCardPandL
+{
+    [BepInPlugin("com.wubzzey.pandlplugin", "PackPandL", "1.0.0")]
+    [BepInProcess("Card Shop Simulator.exe")] // Only run plugin with Card Shop Sim
+    public class Plugin : BaseUnityPlugin
+    {
+        // Config Dropdown Menus and Bools
+        internal ConfigEntry<GodPackMode> godPackSelection;
+        internal ConfigEntry<bool> freeShopItems;
+        internal ConfigEntry<EDecoObject> spawnDecoSelection;
+
+        // Config Hotkeys
+        private ConfigEntry<KeyboardShortcut> SpawnDecoHK { get; set; }
+        private ConfigEntry<KeyboardShortcut> SpawnMoneyHK { get; set; }
+        private ConfigEntry<KeyboardShortcut> RemoveMoneyHK { get; set; }
+        private ConfigEntry<KeyboardShortcut> ShopLevelUpHK { get; set; }
+
+        internal static Plugin Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            // Bind config entries to menu
+            godPackSelection = Config.Bind("Gameplay", "Pack Odds Selection",
+             GodPackMode.Normal, "All packs will open with this type");
+
+            spawnDecoSelection = Config.Bind("Gameplay", "Spawn this Decoration (Shift+O)",
+             EDecoObject.None, "Use O to spawn");
+
+            freeShopItems = Config.Bind("Gameplay", "Free MyDIY objects", false,
+               new ConfigDescription("Toggle free MyDIY objects"));
+
+            //Hotkeys
+            SpawnDecoHK = Config.Bind("Hotkeys", "Spawn Decor Selection",
+                new KeyboardShortcut(KeyCode.O));
+
+            SpawnMoneyHK = Config.Bind("Hotkeys", "Add $5000 (Shift+P)",
+                new KeyboardShortcut(KeyCode.P, KeyCode.LeftShift));
+
+            RemoveMoneyHK = Config.Bind("Hotkeys", "Remove $5000 (Alt+P)",
+                new KeyboardShortcut(KeyCode.P, KeyCode.LeftAlt));
+
+            ShopLevelUpHK = Config.Bind("Hotkeys", "Level Up (Shift+L)",
+                new KeyboardShortcut(KeyCode.L, KeyCode.LeftShift));
+
+            // initialize harmony
+            var harmony = new Harmony("com.wubzzey.pandlplugin");
+            harmony.PatchAll();
+            HarmonyFileLog.Enabled = true;
+        }
+        private void Update()
+        {
+            {
+                // Check for hotkey presses
+                if (SpawnDecoHK.Value.IsDown())
+                {
+                    EDecoObject spawnDecoConfirm = Instance.spawnDecoSelection.Value;
+                    ShelfManager.SpawnDecoObjectOnHand(spawnDecoConfirm);
+                }
+                if (SpawnMoneyHK.Value.IsDown())
+                {
+                    var coinEvent = new CEventPlayer_AddCoin(5000, true);
+                    CEventManager.QueueEvent(coinEvent);
+                }
+                if (RemoveMoneyHK.Value.IsDown())
+                {
+                    var coinEvent = new CEventPlayer_ReduceCoin(5000, true);
+                    CEventManager.QueueEvent(coinEvent);
+                }
+                if (ShopLevelUpHK.Value.IsDown())
+                {
+
+                }
+
+            }
+        }
+    }
+    //used for godpack dropdown menu
+    public enum GodPackMode
+    {
+        Normal = 0,
+        FirstEdition = 1,
+        Silver = 2,
+        Gold = 3,
+        EX = 4,
+        FullArt = 5,
+        FoilFirstEdition = 6,
+        FoilSilver = 7,
+        FoilGold = 8,
+        FoilEX = 9,
+        FoilFullArt = 10,
+        AllFoil = 12,
+        Ghost = 20 // outside of normal roll index (overrides different value)
+    }
+}
